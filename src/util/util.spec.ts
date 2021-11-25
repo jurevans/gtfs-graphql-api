7edeca6bc15f154965bce8aada583ead3d42c787
Justin Evans <jurevans@gmail.com>
2021-11-25 08:08:38 -0500

    Add tests for utility functions

---
+++ src/util/util.spec.ts
@@ -0,0 +1,43 @@
+import { formatCacheKey, getCurrentDay } from 'util/';
+import { CacheKeyPrefix } from 'constants/';
+
+describe('formatCacheKey', () => {
+  it('should return correctly formatted keys', () => {
+    const expected = 'routes?feedIndex=1&routeId=A';
+
+    const actual = formatCacheKey(CacheKeyPrefix.ROUTES, {
+      feedIndex: 1,
+      routeId: 'A',
+    });
+
+    expect(actual).toBe(expected);
+  });
+
+  it('should remove parameters with undefined values', () => {
+    const expected = 'routes?feedIndex=1';
+
+    const actual = formatCacheKey(CacheKeyPrefix.ROUTES, {
+      feedIndex: 1,
+      routeId: null,
+    });
+
+    expect(actual).toBe(expected);
+  });
+});
+
+describe('getCurrentDay', () => {
+  it('should return the correct day given an index', () => {
+    const daysOfWeek = [
+      'sunday',
+      'monday',
+      'tuesday',
+      'wednesday',
+      'thursday',
+      'friday',
+      'saturday',
+    ];
+    const today = daysOfWeek[new Date().getDay()];
+
+    expect(getCurrentDay()).toBe(today);
+  });
+});
