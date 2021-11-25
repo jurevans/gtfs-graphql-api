f1afe9a462e286fe3544026a5b3bf87ca03fae89
Justin Evans <jurevans@gmail.com>
2021-11-25 05:53:52 -0500

    Get new tests to pass

--- src/routes/routes.resolver.spec.ts
+++ src/routes/routes.resolver.spec.ts
@@ -1,13 +1,30 @@
+import { CACHE_MANAGER } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Route } from 'entities/route.entity';
 import { RoutesResolver } from './routes.resolver';
 import { RoutesService } from './routes.service';
 
 describe('RoutesResolver', () => {
   let resolver: RoutesResolver;
 
+  const mockCacheManager = {};
+  const mockRoutesRepository = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoutesResolver, RoutesService],
+      providers: [
+        RoutesResolver,
+        RoutesService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: mockCacheManager,
+        },
+        {
+          provide: getRepositoryToken(Route),
+          useValue: mockRoutesRepository,
+        },
+      ],
     }).compile();
 
     resolver = module.get<RoutesResolver>(RoutesResolver);
--- src/routes/routes.service.spec.ts
+++ src/routes/routes.service.spec.ts
@@ -1,12 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CACHE_MANAGER } from '@nestjs/common';
 import { RoutesService } from './routes.service';
+import { Route } from 'entities/route.entity';
 
 describe('RoutesService', () => {
   let service: RoutesService;
 
+  const mockRoute = {
+    routeId: 'A',
+  };
+  const mockRoutesRepository = {
+    find: jest.fn().mockImplementation((): Promise<Route[]> => {
+      return Promise.resolve([mockRoute as Route]);
+    }),
+    findOne: jest.fn().mockImplementation((): Promise<Route> => {
+      return Promise.resolve(mockRoute as Route);
+    }),
+  };
+
+  const mockCacheManager = {
+    get: jest.fn().mockImplementation((): Promise<Route[]> => {
+      return Promise.resolve([mockRoute as Route]);
+    }),
+    set: jest.fn().mockImplementation((): void => {
+      return;
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoutesService],
+      providers: [
+        RoutesService,
+        {
+          provide: getRepositoryToken(Route),
+          useValue: mockRoutesRepository,
+        },
+        {
+          provide: CACHE_MANAGER,
+          useValue: mockCacheManager,
+        },
+      ],
     }).compile();
 
     service = module.get<RoutesService>(RoutesService);
